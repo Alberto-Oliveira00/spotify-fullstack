e95316333953aea54470f0f224ea284585780513
Alberto Oliveira <alberto.olv00@gmail.com>
2025-02-17 14:57:24 -0300

    ajuste no process env

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -2,9 +2,8 @@
 import "dotenv/config";
 import axios from "axios";
 
-// eslint-disable-next-line no-undef
-const { NODE_ENV } = process.env;
-const URL = NODE_ENV === "development" ? "http://localhost:3000/api" : "/api";
+// const { NODE_ENV } = process.env;
+const URL = "https://spotify-fullstack-1.onrender.com/api";
 
 const responseArtists = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
